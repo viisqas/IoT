a2c8579b531b0d0ce512d11ae0a7edb81238cf63
ismglv <ismglvb@yandex.ru>
2019-03-08 19:10:14 +0300

    wip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import sys
+import RPi.GPIO as GPIO
+import time
 
 import Adafruit_DHT
 
@@ -8,17 +10,11 @@ import Adafruit_DHT
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-else:
-    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-    sys.exit(1)
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#sensor 11 and pin 4
+humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -33,3 +29,33 @@ else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
 
+
+LedPin = 11    # pin11
+
+def setup():
+        GPIO.setmode(GPIO.BOARD)       # Set the board mode to numbers pins by physical location
+        GPIO.setup(LedPin, GPIO.OUT)   # Set pin mode as output
+        GPIO.output(LedPin, GPIO.HIGH) # Set pin to high(+3.3V) to off the led
+
+def loop():
+        while True:
+                humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+                if temperature < 30.0:
+                    GPIO.output(LedPin, GPIO.LOW)   # led on
+                    time.sleep(1.0)                 # wait 1 sec
+                else:
+                    GPIO.output(LedPin, GPIO.HIGH)  # led off
+                time.sleep(5.0)                 # wait 1 sec
+
+def destroy():
+
+        GPIO.output(LedPin, GPIO.HIGH)     # led off
+        GPIO.cleanup()                     # Release resource
+
+
+if __name__ == '__main__':     # Program start from here
+        setup()
+        try:
+                loop()
+        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the destroy() will be  executed.
+                destroy()
